69b00280ff191841b36132fa6f44c71278391245
Evgeni Tsonev <evgeni_136@abv.bg>
2024-03-05 23:57:53 +0200

    Implement SnsPublisher

---
+++ aws-fundamentals/4.Sns/SnsPublisher/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using SnsPublisher;
+
+CustomerCreated customer = new()
+{
+    Id = Guid.NewGuid(),
+    Email = "evgeni@tsonev.com",
+    FullName = "Evgeni Tsonev",
+    DateOfBirth = new DateTime(1999, 2, 23),
+    GitHubUsername = "evgenitsonev"
+};
+
+var snsClient = new AmazonSimpleNotificationServiceClient();
+
+var topicArnResponse = await snsClient.FindTopicAsync("customers");
+
+var publishRequest = new PublishRequest
+{
+    TopicArn = topicArnResponse.TopicArn,
+    Message = JsonSerializer.Serialize(customer),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        {
+            "MessageType", new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = nameof(CustomerCreated)
+            }
+        }
+    }
+};
+
+var response = await snsClient.PublishAsync(publishRequest);
