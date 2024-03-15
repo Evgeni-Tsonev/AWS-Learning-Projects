34db319cb6042480738d1cdb57e2cacd2b758eff
Evgeni Tsonev <evgeni_136@abv.bg>
2024-03-15 21:51:56 +0200

    Implement data upload

--- aws-fundamentals/6.S3/S3Playground/Program.cs
+++ aws-fundamentals/6.S3/S3Playground/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine();
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+
+var s33Client = new AmazonS3Client();
+
+await using var imputStream = new FileStream("./face.jpg", FileMode.Open, FileAccess.Read);
+
+var putObjectRequest = new PutObjectRequest
+{
+    BucketName = "evg-aws-course",
+    Key = "images/face.jpg",
+    ContentType = "image/jpeg",
+    InputStream = imputStream
+};
+
+await s33Client.PutObjectAsync(putObjectRequest);
