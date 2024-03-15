30972690aaf8aef42c993cc7ebdcaee141f56676
Evgeni Tsonev <evgeni_136@abv.bg>
2024-03-15 22:02:50 +0200

    Implement data reading

--- aws-fundamentals/6.S3/S3Playground/Program.cs
+++ aws-fundamentals/6.S3/S3Playground/Program.cs
@@ -1,16 +1,33 @@
 ﻿using Amazon.S3;
 using Amazon.S3.Model;
+using System.Text;
 
 var s33Client = new AmazonS3Client();
 
-await using var imputStream = new FileStream("./face.jpg", FileMode.Open, FileAccess.Read);
-
-var putObjectRequest = new PutObjectRequest
+var getObjectRequest = new GetObjectRequest
 {
     BucketName = "evg-aws-course",
-    Key = "images/face.jpg",
-    ContentType = "image/jpeg",
-    InputStream = imputStream
+    Key = "files/movies.csv",
 };
 
-await s33Client.PutObjectAsync(putObjectRequest);
+var response = await s33Client.GetObjectAsync(getObjectRequest);
+
+using var ms = new MemoryStream();
+response.ResponseStream.CopyTo(ms);
+
+var text = Encoding.Default.GetString(ms.ToArray());
+
+Console.WriteLine(text);
+
+
+//await using var imputStream = new FileStream("./face.jpg", FileMode.Open, FileAccess.Read);
+
+//var putObjectRequest = new PutObjectRequest
+//{
+//    BucketName = "evg-aws-course",
+//    Key = "files/movies.csv",
+//    ContentType = "text/csv",
+//    InputStream = imputStream
+//};
+
+//await s33Client.PutObjectAsync(putObjectRequest);
